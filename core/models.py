9fc86826c42b57a64a8d383e4ff7bcf170dfad0e
Maycon Ferreira <mayconferr.18@gmail.com>
2023-01-05 16:25:50 -0300

    Added contact model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Contact(models.Model):
+    fullname = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.fullname
